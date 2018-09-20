9789945d72dac2c292cdfd452e09d68be926de75
John <johnhanacek@gmail.com>
2018-09-20 11:58:12 -0700

    using visibility instead

--- helioscene.js
+++ helioscene.js
@@ -2,14 +2,14 @@ function heliodetect(){
   if (window.mlWorld) {
     // this is the Helio browser and is capable of rendering spatialized content
 function hide(){
-    document.getElementByClassName("astro2d").display = "none";
+    document.getElementByClassName("astro2d").visibility = "none";
 }
 console.log('helio detected');
 
   } else {
     // this is not Helio
 function show(){
-document.getElementByClassName("astro2d").display = "block";
+document.getElementByClassName("astro2d").visibility = "block";
   }
   console.log('twoD browser');
 }
