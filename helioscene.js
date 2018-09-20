aa82a6e1a7cd49955b5447bbee28a39a2095e631
John <johnhanacek@gmail.com>
2018-09-20 12:11:55 -0700

    removing nested functions

--- helioscene.js
+++ helioscene.js
@@ -1,16 +1,15 @@
 function heliodetect(){
   if (window.mlWorld) {
     // this is the Helio browser and is capable of rendering spatialized content
-function hide(){
+
     document.getElementByClassName("astro2d").style.visibility = "none";
-}
+
 console.log('helio detected');
 
   } else {
     // this is not Helio
-function show(){
 document.getElementByClassName("astro2d").style.visibility = "block";
-  }
+  
   console.log('twoD browser');
 }
 }
