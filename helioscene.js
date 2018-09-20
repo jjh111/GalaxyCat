c81fe2608c78d7c24468633d6b5b1d0c04ccb932
John <johnhanacek@gmail.com>
2018-09-20 12:08:22 -0700

    adding style to .visibility

--- helioscene.js
+++ helioscene.js
@@ -2,14 +2,14 @@ function heliodetect(){
   if (window.mlWorld) {
     // this is the Helio browser and is capable of rendering spatialized content
 function hide(){
-    document.getElementByClassName("astro2d").visibility = "none";
+    document.getElementByClassName("astro2d").style.visibility = "none";
 }
 console.log('helio detected');
 
   } else {
     // this is not Helio
 function show(){
-document.getElementByClassName("astro2d").visibility = "block";
+document.getElementByClassName("astro2d").style.visibility = "block";
   }
   console.log('twoD browser');
 }
