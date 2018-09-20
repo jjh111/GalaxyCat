3ffe20986344aa54e747c8f70cccd2cb2cdd97fb
John <johnhanacek@gmail.com>
2018-09-20 12:19:25 -0700

    removing if statement function holder

--- helioscene.js
+++ helioscene.js
@@ -1,15 +1,14 @@
-function heliodetect(){
+
   if (window.mlWorld) {
     // this is the Helio browser and is capable of rendering spatialized content
 
     document.getElementByClassName("astro2d").style.visibility = "none";
 
-console.log('helio detected');
+    console.log('helio detected');
 
   } else {
     // this is not Helio
-document.getElementByClassName("astro2d").style.visibility = "block";
-  
-  console.log('twoD browser');
-}
+    document.getElementByClassName("astro2d").style.visibility = "block";
+
+    console.log('twoD browser');
 }
